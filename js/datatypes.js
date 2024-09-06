680184d115a95d42db880b59edc852ba4222936a
Bhavya2065 <sanjivdani1@gmail.com>
2024-09-06 05:30:35 +0000

    datatypes

---
+++ js/datatypes.js
@@ -0,0 +1,22 @@
+"use strict"; // treat all code as newer version
+
+let name = "Bhavya" // String 
+let age = 19 // number
+let isLoggedin = false // boolean
+let a = null // contain empty field / value doesn`t assign (Standalone value)
+let b = undefined // cannot contain any value
+
+/* Other datatypes:
+        1.symbol
+        2.bigint
+        3.object (most important)
+*/
+
+console.table([name,age,isLoggedin,a,b])
+
+// "typeof" is used for knowing the datatype of any value.
+console.log(typeof "Bhavya");
+console.log(typeof 19);
+console.log(typeof false);
+console.log(typeof null); // by default the typeof null is object. */ interview Ouestion /*
+console.log(typeof undefined); // by default the typeof undefined is undefined. */ interview Ouestion /*
---
+++ js/variables.js
@@ -0,0 +1,27 @@
+const id = 20003737 
+// Value is constant
+
+let name = 'Bhavya' 
+// value can be updated
+
+var age = 13 
+// it doesn`t use nowdays
+
+dob = '13/12/2002' 
+// this is also a type of assigning value but cannot use anywhere.
+
+let account; 
+// only assigning the value
+
+// id = 6534765 (cannot assign other value because of const variable)
+name = 'Sanjiv'
+
+console.log(id); 
+// to print id 
+
+
+/* The unique idea to print all information in one table form */
+console.table([id,name,age,dob,account]);
+
+
+/* Note: Only numbers are preferable without singlecod (''), not string or any character. */
