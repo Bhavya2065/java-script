809d6e6b3a4e5d51c4f7f7ef0d578b344b44ef80
Bhavya2065 <sanjivdani1@gmail.com>
2025-03-07 05:02:41 +0000

    how js works behind the scene

---
+++ js/execution_of_java_programm.js
@@ -0,0 +1,18 @@
+// Javascript runs in two phases: 
+// (1) Memory creation phase => In this phase only allocation of memory to variable Occure
+// (2) Execution phase => execution such as +,- (mathematical function) and all logics are executed
+
+// First javascript convert the code into global execution context using this variable
+// every environment have their different global E.C. 
+
+// Js runs on single threded environment
+
+// *********************** Interview Questions: ******************************
+
+// for browser what is the global e.c.?
+// Answer: Window Object
+
+// How many e.c. are there?
+// global e.c.
+// functional e.c.
+// eval e.c. (for mongo)
