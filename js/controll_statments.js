d52ca199d4eaabe045e47d07dc32d721f6b4aa78
Bhavya2065 <sanjivdani1@gmail.com>
2025-03-07 06:34:08 +0000

    normal statments

---
+++ js/controll_statments.js
@@ -0,0 +1,18 @@
+const month = "march"
+switch (month) {
+    case "January":
+        console.log(1);
+        break;
+    case "feb":
+        console.log(2);
+        break;
+    case 'march':
+        console.log(3);
+        break;
+    case "april":
+        console.log(4);
+        break;
+    default:
+        console.log(5);
+        break;
+}
