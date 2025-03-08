5383795e115ab3b24984f10bcd87dc7d12ee5134
Bhavya2065 <sanjivdani1@gmail.com>
2025-03-08 17:09:41 +0000

    loops

---
+++ js/loops.js
@@ -0,0 +1,15 @@
+// for (let i = 0; i < 10; i++) {
+//     const element = i;
+//     console.log(element);   
+// }
+// console.log();
+
+// How to print tables using for loop
+
+// for (let i = 1; i < 51; i++) {
+//     for (let j = 1; j < 11; j++) {
+//         console.log(`${i} * ${j} = ${i*j}`);
+//     }
+//     console.log();
+// }
+
